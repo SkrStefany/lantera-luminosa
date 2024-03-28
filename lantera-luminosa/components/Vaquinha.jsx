da8ace3670d4c45c2d734a3965346eb73aaef7d5
Stefany De Souza Matsumoto <99624243+SkrStefany@users.noreply.github.com>
2024-03-28 15:06:21 -0300

    Texto colocado

---
+++ lantera-luminosa/components/Vaquinha.jsx
@@ -0,0 +1,32 @@
+const Vaquinha = () => {
+  return (
+    <div className="flex flex-col justify-center text-center mx-auto max-w-[1100px] items-center font-primary py-16">
+  <p className="mb-12 text-4xl">Contribua com a nossa Vaquinha</p>
+  <p className="mb-12 text-2xl">
+
+  Caros Amigos e Apoiadores, <br />
+  Neste momento crucial, convidamos você a se unir a nós em uma
+  causa que transcende fronteiras e toca o cerne da humanidade: <br /> a proteção das mulheres e de seus filhos contra o abuso. <br />
+  Em todo o mundo, milhões de mulheres e crianças enfrentam diariamente situações de abuso físico, emocional e sexual. Essas realidades sombrias não podem ser ignoradas. No entanto, a esperança brilha através de organizações como a nossa,
+  dedicadas a lutar incansavelmente por um futuro onde essas formas de violência sejam apenas uma lembrança do passado. <br />
+  Nossa ONG tem sido uma voz ativa na defesa dos direitos das mulheres e na prestação de apoio vital para aquelas que sofreram abuso. Trabalhamos incansavelmente para oferecer abrigo, aconselhamento, recursos legais e educação para capacitar
+  mulheres a romperem o ciclo da violência e reconstruírem suas vidas com dignidade e esperança. <br />
+  No entanto, nossa missão só é possível com o apoio generoso daqueles que compartilham de nossa visão de um mundo onde
+  todas as mulheres e crianças possam viver livres do medo e da opressão. <br />
+  É por isso que lançamos esta vaquinha: para ampliar nosso alcance, fortalecer nossos programas e garantir que mais mulheres e crianças
+  em situação de vulnerabilidade recebam o apoio de que tanto necessitam. <br />
+  Cada contribuição, por menor que seja, faz a diferença. Com sua ajuda, podemos oferecer um refúgio seguro para aquelas que fogem da violência, proporcionar terapia
+  e apoio emocional para curar traumas, e educar comunidades sobre a importância do respeito e da igualdade de gênero.
+  Por favor, una-se a nós nesta missão vital. Sua doação não apenas transforma vidas, mas também envia uma mensagem
+  poderosa de solidariedade e esperança para todas as mulheres e crianças que lutam para superar o abuso.
+  Juntos, podemos construir um futuro mais seguro e justo para todos. <br />
+  Obrigado pelo seu apoio
+
+
+  </p>
+  <a href="" className="inline-block py-3 text-lg font-primary text-center text-yellow bg-green rounded-lg px-7 lg:px-5 lg:py-3 ">Acesse clicando aqui!</a>
+  </div>
+  )
+};
+
+export default Vaquinha;
